8ae4e2de0f14bc266df1236d023383ef3f879d34
Kateryna Aleshchenko <katarina_serg@ukr.net>
2016-04-11 10:53:51 +0300

    edited

--- Homework _JS_1/js/script.js
+++ Homework _JS_1/js/script.js
@@ -1,31 +1,32 @@
-var a = prompt('Укажите, пожалуйста, число');
-var n = prompt ('Укажите, пожалуйста, степень для этого числа');
+var a = prompt('Укажите, пожалуйста, число', '');
+var n = prompt('Укажите, пожалуйста, степень для этого числа', '');
+pow(a, n);
 
-function pow (a, n) {
-  if (a==0 && n==-1 ) {
-    console.log('∞ Бесконечность');
+function pow(a, n) {
+  if (a === 0 && n === -1 ) {
+    console.log('Бесконечность');
     return;
-  } else if (a!=0 && n==0) {
+  } else if (a !== 0 && n === 0) {
     console.log ('1');
     return;
-  } else if (a==0 && n!=0) {
+  } else if (a === 0 && n !== 0) {
     console.log ('0');
     return;
-  } else if (a==0 && n==0) {
+  } else if (a === 0 && n === 0) {
     console.log('Неопределенность');
     return;
+
   } else {
-    if (n<0) {
-    n=-n;
-    a=1/a;
-  }
-  var b = a;
-  for (i=1; i<n; i++) {
-      b*=a;
-  }
+      if (n < 0) {
+      n = -n;
+      a = 1/a;
+    }
+
+    var b = a;
+    for (i = 1; i < n; i++) {
+        b *= a;
+    }
 }
+
   console.log(b);
 }
-
-
-pow (a, n);
---
+++ Homework_JS_2/js/script.js
@@ -0,0 +1,15 @@
+
+var arr = [];
+for(var i = 1; i<= 5; i++) {
+  var a = prompt ('Напишите любое имя, пожалуйста', '');
+  arr.push(a);
+}
+
+var userName = prompt ('Как Вас зовут?', '');
+
+for (var i = 0; i < arr.length && userName != arr[i]; i++) {}
+if (i < arr.length) {
+  alert (userName + ', Вы успешно вошли');
+} else {
+  alert ('Произошла ошибка');
+}
