5d54eff88ff369e123c5e6bdf38c5c13b1fe4983
Kateryna Aleshchenko <katarina_serg@ukr.net>
2016-05-16 23:08:35 +0300

    edited

--- Homework_JS_7-8/js/script.js
+++ Homework_JS_7-8/js/script.js
@@ -5,62 +5,49 @@ $(function(){
         $(this).addClass('active');
         var tab = $(this).attr('href');
         $('.tabsContent').not(tab).css({'display':'none'});
-        $(tab).fadeIn(100);
+        $(tab).fadeIn(4);
     });
-
 });
 
 $(function(){
-
-          $(".menu li a").on("click", function(){
-            $(".menu li a").removeClass("highlighted"); //удаляем класс во всех вкладках
-            $(this).addClass("highlighted"); //добавляем класс текущей (нажатой)
-
-          });
-
-
-
-          $('.menu li').hover(function(){
-            $(this).css({
+    function highlight() {
+      $('li:not(.highlighted)').hover(function(){
+          $(this).css({
               borderColor: "#A02C00"
-            });
-          }, function(){
-              $(this).css({
-                borderColor: "transparent"
-              });
-            });
+          });
+      }, function(){
+          $(this).css({
+              borderColor: "transparent"
+          });
+      });
+    }
+    highlight();
 
+    $('.menu li').click(function(){
+        $('.menu li').removeClass('highlighted');
+        $(this).addClass('highlighted');
+        highlight();
+        $(this).off( "mouseenter mouseleave" ).css("borderColor", "transparent");
+
+    });
 });
-
-
-
-// $(function(){
-//   $(".menu li a").on("click", function(){
-//     $(".menu li a").removeClass("highlighted"); //удаляем класс во всех вкладках
-//     $(this).addClass("highlighted"); //добавляем класс текущей (нажатой)
-//
-//   });
-// });
 
 $(function(){
     $('input[type=text]').hover(function(){
-      $(this).next('em').animate ({
-          opacity: "show"
-      }, "slow");
+        $(this).next('em').animate ({
+            opacity: "show"
+        }, "slow");
     }, function(){
-      $(this).next('em').animate ({
-          opacity: "hide"
-      }, "slow");
-    }
-  );
-}
-);
+        $(this).next('em').animate ({
+            opacity: "hide"
+        }, "slow");
+    });
+});
 
 $(function(){
     $('input[type=button]').click(function(){
-      $('em').animate ({
-          opacity: "show"
-      }, "slow");
-
+        $('em').animate ({
+            opacity: "show"
+        }, "slow");
     });
 });
