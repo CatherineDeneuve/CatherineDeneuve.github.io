74c547dd3cb2af5fba5c58b29d28604fba9e0dff
Kateryna Aleshchenko <katarina_serg@ukr.net>
2016-05-17 13:39:50 +0300

    edited

--- Homework_JS_7-8/js/script.js
+++ Homework_JS_7-8/js/script.js
@@ -1,3 +1,4 @@
+
 $(function(){
     $('.menu a').on('click', function(e) {
         e.preventDefault();
@@ -13,23 +14,32 @@ $(function(){
     function highlight() {
       $('li:not(.highlighted)').hover(function(){
           $(this).css({
-              borderColor: "#A02C00"
+              borderColor: "rgba(0,0,0,0.25)",
+              boxShadow: "inset 0 0 5px rgba(0,0,0, 0.4), rgba(255,255,255,0.1) 0 1px 0",
+              textShadow: "rgba(0, 0, 0, 0.796875) 0 -1px 0, rgba(255, 255, 255, 0.296875) 0 0 10px"
           });
       }, function(){
           $(this).css({
+              boxShadow: "none",
+              textShadow: "none",
               borderColor: "transparent"
           });
       });
     }
     highlight();
 
+
+
     $('.menu li').click(function(){
         $('.menu li').removeClass('highlighted');
         $(this).addClass('highlighted');
         highlight();
-        $(this).off( "mouseenter mouseleave" ).css("borderColor", "transparent");
-
+        $(this).off( "mouseenter mouseleave" ).css({
+            borderColor: "transparent",
+            boxShadow: "none"
+        });
     });
+
 });
 
 $(function(){
