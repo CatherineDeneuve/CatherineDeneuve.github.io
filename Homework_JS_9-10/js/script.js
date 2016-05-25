c31741e2b3febf95df40000ed490fe1bc5a2e818
Kateryna Aleshchenko <katarina_serg@ukr.net>
2016-05-25 10:26:40 +0300

    edited

--- Homework_JS_9-10/js/script.js
+++ Homework_JS_9-10/js/script.js
@@ -45,77 +45,50 @@ $(function () {
 });
 
 // Карусель
-
-$(function() {
-
-// Инициализация слайдера
-
-$('.jcarousel').jcarousel({
-  // Базовые настройки скрипта пишутся здесь
-
+$(window).load(function(){
+    $(function() {
+      // Инициализация слайдера
+      $('.jcarousel').jcarousel();
+      // Прокрутка слайдера
+      // Кнопка PREV
+      $('.jcarousel-prev')
+      // Триггер класса inactive
+      .on('jcarouselcontrol:active', function() {
+        $(this).removeClass('inactive');
+      })
+      .on('jcarouselcontrol:inactive', function() {
+        $(this).addClass('inactive');
+      })
+      // Инициализация кнопки PREV
+      .jcarouselControl({
+        target: '-=1'
+      });
+      // Кнопка NEXT
+      $('.jcarousel-next')
+      // Триггер класса inactive
+      .on('jcarouselcontrol:active', function() {
+        $(this).removeClass('inactive');
+      })
+      .on('jcarouselcontrol:inactive', function() {
+        $(this).addClass('inactive');
+      })
+      // Инициализация кнопки NEXT
+      .jcarouselControl({
+        target: '+=1'
+      });
+      // Пагинация слайдера
+      $('.jcarousel-pagination')
+      // Триггер класса active
+      .on('jcarouselpagination:active', 'a', function() {
+        $(this).addClass('active');
+      })
+      .on('jcarouselpagination:inactive', 'a', function() {
+        $(this).removeClass('active');
+      })
+      // Инициализация пагинации
+      .jcarouselPagination();
+    });    
 });
-
-// Прокрутка слайдера
-
-
-// Кнопка PREV
-
-$('.jcarousel-prev')
-
-// Триггер класса inactive
-
-.on('jcarouselcontrol:active', function() {
-  $(this).removeClass('inactive');
-})
-.on('jcarouselcontrol:inactive', function() {
-  $(this).addClass('inactive');
-})
-
-// Инициализация кнопки PREV
-
-.jcarouselControl({
-  target: '-=1'
-});
-
-// Кнопка NEXT
-
-$('.jcarousel-next')
-
-// Триггер класса inactive
-
-.on('jcarouselcontrol:active', function() {
-  $(this).removeClass('inactive');
-})
-.on('jcarouselcontrol:inactive', function() {
-  $(this).addClass('inactive');
-})
-
-// Инициализация кнопки NEXT
-
-.jcarouselControl({
-  target: '+=1'
-});
-
-// Пагинация слайдера
-
-$('.jcarousel-pagination')
-
-// Триггер класса active
-
-.on('jcarouselpagination:active', 'a', function() {
-  $(this).addClass('active');
-})
-.on('jcarouselpagination:inactive', 'a', function() {
-  $(this).removeClass('active');
-})
-
-// Инициализация пагинации
-.jcarouselPagination({
-  item: function(page) {
-    return '<a href="#' + page + '">' + page + '</a>';
-  }
-});
-
 
 
 // Автопрокрутка слайдера
@@ -126,7 +99,7 @@ $('.jcarousel-pagination')
 //     autostart: true
 // });
 
-});
+
 
 // Стилизируем селекты
 
