8e5890becb2c46880967bfc5d63548f6dcfc5187
Kateryna Aleshchenko <katarina_serg@ukr.net>
2016-06-03 15:22:01 +0300

    edited

--- Sticky Header/js/script.js
+++ Sticky Header/js/script.js
@@ -7,22 +7,25 @@ var headerHeight = headers[0].clientHeight;
 var delta = headerHeight + texts[0].clientHeight;
 
 var normalPaddingTop = getComputedStyle(texts[0]).paddingTop;
+function ready() {
+  window.onscroll = function(e) {
+    var offset = window.pageYOffset;
 
-window.onscroll = function(e) {
-  var offset = window.pageYOffset;
+    var stepOffset = headHeight;
 
-  var stepOffset = headHeight;
+    for (var i = 0; i < headers.length; i++){
+      if ((offset >= stepOffset) && (offset < (stepOffset + delta))) {
+        headers[i].style.position = 'fixed';
+        headers[i].style.top = '0px';
+        texts[i].style.paddingTop = parseInt(normalPaddingTop, 10) + headerHeight + 'px';
+      } else {
+        headers[i].style.position = 'static';
+        texts[i].style.paddingTop = normalPaddingTop;
+      }
+      stepOffset += delta;
 
-  for (var i = 0; i < headers.length; i++){
-    if ((offset >= stepOffset) && (offset < (stepOffset + delta))) {
-      headers[i].style.position = 'fixed';
-      headers[i].style.top = '0px';
-      texts[i].style.paddingTop = parseInt(normalPaddingTop, 10) + headerHeight + 'px';
-    } else {
-      headers[i].style.position = 'static';
-      texts[i].style.paddingTop = normalPaddingTop;
     }
-    stepOffset += delta;
+  };
+}
 
-  }
-};
+document.addEventListener("DOMContentLoaded", ready);
