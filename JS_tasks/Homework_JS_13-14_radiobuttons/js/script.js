dd4e4bc237fed67eb93fbd037eb225e854c33a35
Kateryna Aleshchenko <katarina_serg@ukr.net>
2016-06-06 20:17:01 +0300

    edited

--- JS_tasks/Homework_JS_13-14_radiobuttons/js/script.js
+++ JS_tasks/Homework_JS_13-14_radiobuttons/js/script.js
@@ -7,8 +7,10 @@ var label;
 var elems;
 var button;
 var input;
+var y = 0;
 var textnode;
 var inputs = document.getElementsByTagName('input');
+var labels = document.getElementsByTagName('label');
 
 var test = {
         form: document.body,
@@ -57,7 +59,7 @@ var test = {
 
           createInput: function (textLabel) {
             label = document.createElement('label');
-            input = document.createElement('input');
+            // input = document.createElement('input');
             label.innerHTML = '<input>' + textLabel;
             ol.appendChild(label);
             this.getGrid(label);
@@ -67,10 +69,17 @@ var test = {
           changeTypeForInput: function () {
               elems = document.querySelectorAll('input:not([type=button])');
               for (var i = 0; i < elems.length; i++){
-                elems[i].setAttribute('type', 'checkbox');
+                elems[i].setAttribute('type', 'radio');
+                // for ( var g = 0; g < myObj[0].answers[0].length; g++)
+                // elems[i].setAttribute('name', y);
+                // console.log(myObj[0].answers[0].length);
               }
-
           },
+
+          // setNameToRadio: function (input, nameInput) {
+          //     input.setAttribute('name', nameInput);
+          //
+          // },
 
           createButton: function () {
               button = document.createElement('button');
@@ -102,13 +111,14 @@ $(function () {
 });
 
 
-
-
-
-
 var z;
 var a = 0;
 var result = 0;
+var answerCorrect;
+var answerWrong;
+var question;
+var answer;
+
 function handler() {
 
 
@@ -117,27 +127,60 @@ function handler() {
 
     for (var b = 0; b < myObj[k].answers.length; b++) {
 
-        if (inputs[a++].checked == myObj[k].answers[b].correct){
+        if (inputs[a].checked == myObj[k].answers[b].correct){
           z++;
+              if (inputs[a].checked === true) {
+                textResult = myObj[k].answers[b].text + 'правильный!';
+              }
+        } else {
+          if (myObj[k].answers[b].correct === true)
+          textResult = 'неправильный';
         }
+        a++;
     }
     if(z < myObj[k].answers.length){
-      console.log("Неправильно");
 
+      wrong = document.createElement('ol');
+      question = document.createElement('li');
+      answer = document.createElement('li');
+      answer.classList.add('wrong');
+      question.innerHTML = myObj[k].text;
+      answer.innerHTML = "Ваш ответ: " + textResult;
+      textBam.appendChild(wrong);
+      wrong.appendChild(question);
+      wrong.appendChild(answer);
+
+
+      // wrong = document.createElement('li');
+      // wrong.classList.add('wrong');
+      // wrong.innerHTML = myObj[k].text + '\n' + "Ваш ответ: " + textResult;
+      // textBam.appendChild(wrong);
     } else {
-      console.log("Правильно");
+
+      right = document.createElement('ol');
+      question = document.createElement('li');
+      answer = document.createElement('li');
+      answer.classList.add('right');
+      question.innerHTML = myObj[k].text;
+      answer.innerHTML = "Ваш ответ: " + textResult;
+      textBam.appendChild(right);
+      right.appendChild(question);
+      right.appendChild(answer);
+
+      // right = document.createElement('li');
+      // right.classList.add('right');
+      // right.innerHTML = myObj[k].text + '\n' + "Ваш ответ: " + textResult;
+      // textBam.appendChild(right);
+
       result++;
     }
   }
 
-  if (result < (myObj.length - 2)) {
-
-    resume.innerHTML = '<p>Вы допустили несколько ошибок</p>';
+  if (result <= (myObj.length - 2)) {
+      resume.innerHTML = '<p>Вы допустили несколько ошибок, к сожалению</p>';
   } else if (result == myObj.length) {
-
-    resume.innerHTML = 'Вы прошли тест без единой ошибки!';
+      resume.innerHTML = 'Вы прошли тест без единой ошибки!';
   } else {
-
     resume.innerHTML = 'Вы допустили всего одну ошибку!';
   }
 }
@@ -146,8 +189,15 @@ function handler() {
     var modalWindow = document.querySelector('.overlay');
     var modal = document.querySelector('.modal');
     var resume = document.createElement('div');
+    resume.classList.add('resume');
     var paragraph = document.querySelector('.questionRepeat');
-    modal.insertBefore(resume, paragraph);
+    var textBam = document.querySelector('.results');
+    var textResult;
+    var wrong;
+    var right;
+
+    modal.insertBefore(resume, textBam);
+
 
     function showModal() {
       modalWindow.style.display = 'block';
@@ -159,11 +209,25 @@ function handler() {
     function resetAnswers(){
       for (var c = 0; c < inputs.length; c++){
         inputs[c].checked = false;
-        console.log(inputs[c]);
+
       }
+
       result = 0;
       z = 0;
       a = 0;
+
+    }
+
+    var array;
+    var arrayLength;
+
+
+    function resetText() {
+      array = textBam.getElementsByTagName('*');
+      arrayLength = array.length;
+      for (var e = 0; e <= arrayLength; e = 0) {
+          textBam.removeChild(array[e]);
+      }
     }
 
     function hideModal(){
@@ -171,5 +235,7 @@ function handler() {
     }
 
     var ok = document.querySelector('.btn');
+
+    ok.addEventListener('click', resetText);
     ok.addEventListener('click', resetAnswers);
     ok.addEventListener('click', hideModal);
