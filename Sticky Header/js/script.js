37475b40cf4e855aae53f0f41cfae1d7b3f5c945
Kateryna Aleshchenko <katarina_serg@ukr.net>
2016-06-10 14:34:00 +0300

    edited sticky header

--- Sticky Header/js/script.js
+++ Sticky Header/js/script.js
@@ -1,10 +1,10 @@
-// var browser;
-// (function browserDetectJS() {
-//   if (window.sidebar) {
-//     browser = "Firefox";
-//     return browser;
-//   }
-// }());
+var browser;
+(function browserDetectJS() {
+  if (window.sidebar) {
+    browser = "Firefox";
+    return browser;
+  }
+}());
 
 var headers = document.querySelectorAll('.header');
 var texts = document.querySelectorAll('.text');
@@ -15,9 +15,38 @@ var delta = headerHeight + texts[0].clientHeight;
 
 var normalPaddingTop = getComputedStyle(texts[0]).paddingTop;
 console.log(normalPaddingTop);
+// var firstHeader = document.querySelector('.header1');
+// var coords = firstHeader.getBoundingClientRect();
+// var coordsTop = coords.y;
+// console.log(coordsTop);
+
+if (browser == "Firefox"){
+  setInterval (function scrollFirefoxScroll() {
+    var firstHeader = document.querySelector('.header1');
+    console.log(firstHeader);
+    var coords = firstHeader.getBoundingClientRect();
+    var coordsTop = coords.y;
+    console.log(coordsTop);
+
+
+
+    if ((coordsTop <= -40) && (coordsTop >= -465)) {
+      firstHeader.style.position = 'fixed';
+      firstHeader.style.top = '0px';
+    }
+  }, 500);
+    // else if ((coordsTop > -40) || (coordsTop < -465)){
+    //   firstHeader.style.position = 'static';
+    //   firstHeader.style.top = '50px';
+    // }
+  // }
+};
+
+
+// })();
 
 // if (browser !== "Firefox") {
-window.onscroll = setInterval(function(e) {
+window.onscroll = function(e) {
     var offset = window.pageYOffset;
 
     var stepOffset = headHeight;
@@ -34,5 +63,5 @@ window.onscroll = setInterval(function(e) {
       stepOffset += delta;
 
     }
-  }, 4);
+  };
 // }
