79836b33005f9e57614c68164b7789aefbf9e7f1
Kateryna Aleshchenko <katarina_serg@ukr.net>
2016-07-23 20:12:47 +0300

    added responsive

--- Final_exam/src/js/script.js
+++ Final_exam/src/js/script.js
@@ -1,16 +1,30 @@
 $(function () {
+  var $container = $('.container');
 
+  function initializeMasonry(){
+          $container.imagesLoaded( function() {
+              $container.masonry({
+                  // Настройки
+                  columnWidth: '.item',
+                  itemSelector: '.item'
+              });
+          });
+  }
 // AJAX-запросы
+
     $('.search').on('click', search);
     $(window).on('load', search);
 
+
     function search(e) {
         e.preventDefault();
-        $('#container img').remove();
+        $container.masonry('destroy');
+        $('.container .item').remove();
         var $searchKey = $('.text').val();
+        // initializeMasonry();
 
         $.ajax({
-            url: 'https://pixabay.com/api/?key=2716174-89a9b125a63640e63bc918f65&q=' + $searchKey + '&image_type=photo' + '&per_page=10',
+            url: 'https://pixabay.com/api/?key=2716174-89a9b125a63640e63bc918f65&q=' + $searchKey + '&per_page=10',
             dataType: 'jsonp',
 
             success: function (data) {
@@ -20,35 +34,49 @@ $(function () {
                   var arr = tags.split(', ');
                   var word = arr[0];
                   var name = '<p>' + word + '</p>';
-                  var img = '<img src="' + $results[i].previewURL + '">' + name;
+                  var img = '<img src="' + $results[i].webformatURL + '">' + name;
                   var div = '<div>' + img + '</div>';
                   $container.append(div);
                   $('.container div').addClass('item');
-                  initializeMasonry();
                 }
+                initializeMasonry();
+
 
             },
             error: function () {
                 alert('Error!');
-                initializeMasonry();
             }
         });
+
 
         $('.text').val('');
     }
 
 
-var $container = $('.container');
-
-function initializeMasonry(){
-        $container.imagesLoaded( function() {
-            $container.masonry({
-                itemSelector: '.item',
-                columnWidth: 300
-                });
-        });
 
 
-}
+// var container = document.querySelector('.container');
+// var msnry;
+// // Инициализация Масонри, после загрузки изображений
+// function initializeMasonry(){
+//   imagesLoaded( container, function() {
+//     msnry = new Masonry( container, {
+//     // Настройки
+//     columnWidth: 200,
+//     itemSelector: '.item'
+//     });
+//   });
+// }
+
+// function initializeMasonry(){
+//         $container.imagesLoaded( function() {
+//             $container.masonry({
+//                 columnWidth: function( containerWidth ) {
+//                     return containerWidth / 3
+//                 }
+//             });
+//         });
+//     }
+
 
 });
