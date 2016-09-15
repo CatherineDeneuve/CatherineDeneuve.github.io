8dfe45db4f7a14e78b4a4099dc448c4079063b5b
Kateryna Aleshchenko <katarina_serg@ukr.net>
2016-09-15 23:13:15 +0300

    added Siteplus task

---
+++ Siteplus/js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    var left = $('.carousel-arrow-left');
+    var right = $('.carousel-arrow-right');
+    var elementsList = $('.carousel-list');
+    var smallHider = $('.carousel-hider-small');
+    var listWidth = $('.carousel-list').width();
+    var img = $('.carousel-element:first').width();
+
+    var currentLeftValue = 0;
+    var currentRightValue = img;
+    var minimumOffset = -(listWidth - img);
+    var maximumOffset = 0;
+    var page = $('.pagination span');
+
+
+  function checkArrow () {
+        if (currentLeftValue == maximumOffset) {
+            left.addClass('inactive');
+        } else {
+            left.removeClass('inactive');
+        }
+        if (currentLeftValue == minimumOffset) {
+            right.addClass('inactive');
+        } else {
+            right.removeClass('inactive');
+        }
+    };
+    checkArrow ();
+
+    function goRight() {
+      if (currentLeftValue != minimumOffset) {
+          currentLeftValue -= img;
+          elementsList.animate({left: currentLeftValue + "px"}, 500);
+          checkArrow();
+      }
+    }
+
+    function goLeft() {
+      if (currentLeftValue != maximumOffset) {
+          currentLeftValue += img;
+          elementsList.animate({left: currentLeftValue + "px"}, 500);
+          checkArrow();
+      }
+    }
+
+    page.click(function scrollSlider() {
+          var pageNumber = $(this).text();
+          page.css({color: '#949494'});
+          $(this).css({color: '#2c2e37'});
+          console.log('img', img);
+          currentLeftValue = -((pageNumber - 1) * img);
+          elementsList.animate({left: currentLeftValue + "px"}, 500);
+          checkArrow ();
+    });
+
+    left.on('click', goLeft);
+    right.on('click', goRight);
+    smallHider.on('click', goRight);
+
+}); //jQuery
