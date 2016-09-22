87d7faf32bee62af842afce1d983b3580001fbba
Kateryna Aleshchenko <katarina_serg@ukr.net>
2016-09-22 12:44:30 +0300

    fixed slider

--- Mobile_phone/js/script.js
+++ Mobile_phone/js/script.js
@@ -1,26 +1,120 @@
 $(document).ready(function() {
 
-    // SLIDER
+    // // SLIDER vertical
+    // var left = $('.icon-left-open-big');
+    // var right = $('.icon-right-open-big');
+    // var elementsList = $('.slider-list');
+    //
+    // var listHeight = $('.slider-list').height();
+    // var slide = $('.carousel-hider').height();
+    //
+    // var currentTopValue = 0;
+    // var minimumOffset = -(listHeight - slide);
+    // var maximumOffset = 0;
+    //
+    //
+    //
+    // function checkArrow() {
+    //     if (currentTopValue == maximumOffset) {
+    //         left.addClass('inactive');
+    //     } else {
+    //         left.removeClass('inactive');
+    //     }
+    //     if (currentTopValue == minimumOffset) {
+    //         right.addClass('inactive');
+    //     } else {
+    //         right.removeClass('inactive');
+    //     }
+    // };
+    // checkArrow();
+    //
+    // function goRight() {
+    //     if (currentTopValue != minimumOffset) {
+    //         currentTopValue -= slide;
+    //         console.log(currentTopValue);
+    //         elementsList.animate({
+    //             top: currentTopValue + "px"
+    //         }, 'fast');
+    //         checkArrow();
+    //         checkSlide();
+    //     }
+    // }
+    //
+    // function goLeft() {
+    //     if (currentTopValue != maximumOffset) {
+    //         currentTopValue += slide;
+    //         elementsList.animate({
+    //             top: currentTopValue + "px"
+    //         }, 'fast');
+    //         checkArrow();
+    //         checkSlide();
+    //
+    //     }
+    // }
+    //
+    // function checkSlide() {
+    //     if (currentTopValue === 0) {
+    //         $('.inputs input:nth-of-type(1)').prop("checked", true);
+    //     } else {
+    //         var number = (-currentTopValue / slide);
+    //         $($('.inputs input')[number]).prop("checked", true);
+    //     }
+    // }
+    // checkSlide();
+    //
+    //
+    // function switchRadiobutton() {
+    //     var inputs = $('.inputs input');
+    //
+    //     var checkedRadiobutton = inputs.index($('.inputs input:checked'));
+    //     if (checkedRadiobutton === 0) {
+    //         currentTopValue = 0;
+    //
+    //         elementsList.animate({
+    //             top: currentTopValue + "px"
+    //         }, 'fast');
+    //         checkArrow();
+    //     } else {
+    //         console.log('checkedRadiobutton', checkedRadiobutton);
+    //         var number = checkedRadiobutton;
+    //         currentTopValue = -(number * slide);
+    //         console.log('currentTopValue', currentTopValue);
+    //         elementsList.animate({
+    //             top: currentTopValue + "px"
+    //         }, 'fast');
+    //         checkArrow();
+    //     }
+    //
+    // }
+    // switchRadiobutton();
+    //
+    //
+    // $('.inputs').on('click', switchRadiobutton);
+    // left.on('click', goLeft);
+    // right.on('click', goRight);
+
+
+    // SLIDER horizontal
     var left = $('.icon-left-open-big');
     var right = $('.icon-right-open-big');
     var elementsList = $('.slider-list');
 
-    var listHeight = $('.slider-list').height();
-    var slide = $('.carousel-hider').height();
+    var listWidth = $('.slider-list').width();
+    var slide = $('.carousel-hider').width();
 
-    var currentTopValue = 0;
-    var minimumOffset = -(listHeight - slide);
+    var currentLeftValue = 0;
+    var minimumOffset = -(listWidth - slide);
     var maximumOffset = 0;
 
 
 
     function checkArrow() {
-        if (currentTopValue == maximumOffset) {
+        if (currentLeftValue == maximumOffset) {
             left.addClass('inactive');
         } else {
             left.removeClass('inactive');
         }
-        if (currentTopValue == minimumOffset) {
+        if (currentLeftValue == minimumOffset) {
             right.addClass('inactive');
         } else {
             right.removeClass('inactive');
@@ -29,23 +123,23 @@ $(document).ready(function() {
     checkArrow();
 
     function goRight() {
-        if (currentTopValue != minimumOffset) {
-            currentTopValue -= slide;
-            console.log(currentTopValue);
+        if (currentLeftValue != minimumOffset) {
+            currentLeftValue -= slide;
+            console.log(currentLeftValue);
             elementsList.animate({
-                top: currentTopValue + "px"
-            }, 'fast');
+                left: currentLeftValue + "px"
+            }, 'slow');
             checkArrow();
             checkSlide();
         }
     }
 
     function goLeft() {
-        if (currentTopValue != maximumOffset) {
-            currentTopValue += slide;
+        if (currentLeftValue != maximumOffset) {
+            currentLeftValue += slide;
             elementsList.animate({
-                top: currentTopValue + "px"
-            }, 'fast');
+                left: currentLeftValue + "px"
+            }, 'slow');
             checkArrow();
             checkSlide();
 
@@ -53,10 +147,10 @@ $(document).ready(function() {
     }
 
     function checkSlide() {
-        if (currentTopValue === 0) {
+        if (currentLeftValue === 0) {
             $('.inputs input:nth-of-type(1)').prop("checked", true);
         } else {
-            var number = (-currentTopValue / slide);
+            var number = (-currentLeftValue / slide);
             $($('.inputs input')[number]).prop("checked", true);
         }
     }
@@ -68,19 +162,19 @@ $(document).ready(function() {
 
         var checkedRadiobutton = inputs.index($('.inputs input:checked'));
         if (checkedRadiobutton === 0) {
-            currentTopValue = 0;
+            currentLeftValue = 0;
 
             elementsList.animate({
-                top: currentTopValue + "px"
+                left: currentLeftValue + "px"
             }, 'fast');
             checkArrow();
         } else {
             console.log('checkedRadiobutton', checkedRadiobutton);
             var number = checkedRadiobutton;
-            currentTopValue = -(number * slide);
-            console.log('currentTopValue', currentTopValue);
+            currentLeftValue = -(number * slide);
+            console.log('currentLeftValue', currentLeftValue);
             elementsList.animate({
-                top: currentTopValue + "px"
+                left: currentLeftValue + "px"
             }, 'fast');
             checkArrow();
         }
@@ -111,4 +205,3 @@ $(document).ready(function() {
 
 
 }); //jQuery
-; //jQuery
