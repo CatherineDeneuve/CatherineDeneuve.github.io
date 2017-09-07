ba20cfeebfe8bdeff86d8db747f3e08ab3cf53aa
Kateryna Aleshchenko <katarina_serg@ukr.net>
2017-09-07 19:15:48 +0200

    addes sprites

--- OOP/script.js
+++ OOP/script.js
@@ -1,72 +1,57 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-  // $('img[src*="fiot_ukr_blue.png"]').fadeIn(6000);
-
-	showIntro();
-
-  function showIntro(){
-		var link = $('link[href="pages/intro.html"]')[0];
-		var content = link.import.querySelector('#intro');
-
-		var div = $('.lr_div')[0];
-		/*var docZone = $('#docZone');
-		docZone.css("display", "none");*/
-		$(div).empty();
-		div.appendChild(content.cloneNode(true));
-	}
-
-  $('.menu a').on('click', function(e) {
-    e.preventDefault();
-    $('.menu .active').removeClass('active');
-    $(this).addClass('active');
-
-
-  });
-
-  $('.dopmat a').on('click', function(e) {
-    e.preventDefault();
-    $('.menu .active').removeClass('active');
-
-
-    var div = $('.lr_div')[0];
-    $(div).empty();
-
-    var objectPdf = document.createElement('embed');
-    objectPdf.setAttribute("src", "files/JCC.pdf#page=1&zoom=96");
-    $('#doc').prop('href', 'files/JCC.pdf');
-    div.appendChild(objectPdf);
-  });
-
-  /*function highlight() {
-    $('li').hover(function() {
-      $(this).addClass('hover');
-    }, function() {
-      $(this).removeClass('hover');
-    });
-  }
-  highlight();*/
-
-
-
-  $('.menu li').click(function() {
-    $('.menu li').removeClass('highlighted');
-    $('.menu li a').addClass('usual');
-    $(this).addClass('highlighted');
-      $('.arrow').css("display", "inline-block");
-    var indLi = $('.menu li').index($(this));
-    var link = $('link[rel="import"]')[indLi];
-    var content = link.import.querySelector('.lr_text');
-
-    var div = $('.lr_div')[0];
-    $(div).empty();
-    div.appendChild(content.cloneNode(true));
-
-  });
-
-  $('.header').click(function() {
-    $('.menu li').removeClass('highlighted');
-    $('.menu .active').removeClass('active');
+    // $('img[src*="fiot_ukr_blue.png"]').fadeIn(6000);
 
     showIntro();
-  });
+
+    function showIntro() {
+        var link = $('link[href="pages/intro.html"]')[0];
+        var content = link.import.querySelector('#intro');
+
+        var div = $('.lr_div')[0];
+
+        $(div).empty();
+        div.appendChild(content.cloneNode(true));
+    }
+
+    $('.menu a').on('click', function (e) {
+        e.preventDefault();
+
+    });
+
+    $('.dopmat a').on('click', function (e) {
+        e.preventDefault();
+        $('.menu .active').removeClass('active');
+
+
+        var div = $('.lr_div')[0];
+        $(div).empty();
+
+        var objectPdf = document.createElement('embed');
+        objectPdf.setAttribute("src", "files/JCC.pdf#page=1&zoom=96");
+        $('#doc').prop('href', 'files/JCC.pdf');
+        div.appendChild(objectPdf);
+    });
+
+
+    $('.menu li').click(function () {
+        $('.menu li').removeClass('highlighted');
+        $('.menu li a').addClass('usual');
+        $(this).addClass('highlighted');
+        var indLi = $('.menu li').index($(this));
+        var link = $('link[rel="import"]')[indLi];
+        var content = link.import.querySelector('.lr_text');
+
+        var div = $('.lr_div')[0];
+        $(div).empty();
+        div.appendChild(content.cloneNode(true));
+
+    });
+
+    $('.header').click(function () {
+        $('.menu li').removeClass('highlighted');
+        $('.menu .active').removeClass('active');
+
+        showIntro();
+    });
 });
